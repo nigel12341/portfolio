5c77bd0c898f6822e7c4962af0e94e69de8ca63f
Nigel Christiaans <nigelchristiaans747@gmail.com>
2023-02-04 19:21:58 +0100

    Create vue project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css"
+import './plugins/bootstrap-vue'
+import "bootstrap/dist/js/bootstrap.js"
+
+createApp(App).use(router).mount('#app')
+
