3014ddcc440e0a3991a2d10e33dc6ea224803f98
Nigel Christiaans <nigelchristiaans747@gmail.com>
2023-02-06 23:53:07 +0100

    Install i18n

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import 'bootstrap'
 import App from './App.vue'
 import router from './router'
+import i18n from './i18n'
 
 
-createApp(App)
+createApp(App).use(i18n)
     .use(router)
     .mount('#app')
