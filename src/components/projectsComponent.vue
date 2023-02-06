85603c4454ac2c9c118e0f775237746c5b5239f3
Nigel Christiaans <nigelchristiaans747@gmail.com>
2023-02-06 23:23:48 +0100

    Added PEWA to projects

--- src/components/projectsComponent.vue
+++ src/components/projectsComponent.vue
@@ -83,6 +83,33 @@
         </svg>
         Github Repository</a>
     </div>
+    <div class="project" id="portfolioWebsite">
+      <img class="projectImage rounded-circle" src="@/assets/selfie.png" alt="Selfie">
+      <h3>Project Enterprise web application</h3>
+      <p>This is a project i did with a team for a grade at school<br>It is a web application for the municipality of Amsterdam<br>It is made with 5 people including me<br>Source code is not available to be viewed.</p>
+      <p>
+        <span class="techBox">Vue</span>
+        <span class="techBox">NodeJS</span>
+        <span class="techBox">HTML</span>
+        <span class="techBox">CSS</span>
+        <span class="techBox">JS</span>
+        <span class="techBox">Bootstrap</span>
+        <span class="techBox">Java</span>
+        <span class="techBox">Spring boot</span>
+        <span class="techBox">Junit</span>
+      </p>
+      <a href="https://ewa-fe-app-production.up.railway.app/#/home"  target="_blank"  class="btn btn-primary PewaButton">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud" viewBox="0 0 16 16">
+          <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+        </svg>
+        Link to website
+      </a>
+      <a class="btn btn-secondary disabled" disabled>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+        </svg>
+        Github Repository (unavailable)</a>
+    </div>
   </div>
 </template>
 
@@ -108,6 +135,10 @@ export default {
 .secondButton {
   margin-left: 10px;
   background-color: var(--secondary-color) !important;
+}
+.PewaButton {
+  background-color: var(--main-color) !important;
+  margin-right: 10px;
 }
 .techBox {
   margin: 3px;
