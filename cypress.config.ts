162386bdbbed0705b8621197694b3ae77542662a
Nigel Christiaans <nigelchristiaans747@gmail.com>
2023-02-09 00:33:55 +0100

    Removed test that no longer works since typing animation is removed. Update cypress config and fixed en.json invalid json

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     },
     specPattern: "tests/e2e/specs/*.cy.{js,jsx,ts,tsx}",
     supportFile: "tests/e2e/support/index.js",
+    baseUrl: "http://localhost:8080"
   },
 
   component: {
